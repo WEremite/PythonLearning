cc1e18afae453eb57ba862c5788b93eb070eab4e
WEremite <weremite@gmail.com>
2021-10-04 18:16:28 +0300

    first commit

---
+++ English Thesaurus/find_word_definition.py
@@ -0,0 +1,36 @@
+import json
+from difflib import get_close_matches
+
+dictionary = json.load(open("data.json"))
+
+
+def find_word_definition(word):
+    lower_word = word.lower()
+    close_matches = get_close_matches(word, dictionary.keys())
+    if lower_word in dictionary:
+        word_definition = dictionary[lower_word]
+        if len(word_definition) == 1:
+            print('Word definition is:')
+        else:
+            print('Word definitions are:')
+
+        count = 1
+        for sentence in word_definition:
+            print(f'{count}) {sentence}', "")
+            count += 1
+
+    elif len(close_matches) > 0:
+        print('Did you mean "%s" instead? [Y]/[n]' % close_matches[0])
+        user_choice = input('')
+        if user_choice.lower() == 'y':
+            find_word_definition(close_matches[0])
+        else:
+            print(
+                f'There is no definition for word "{word}". Please double check it.')
+    else:
+        print(
+            f'There is no definition for word "{word}". Please double check it.')
+
+
+word = input("Write a word and press enter: ")
+find_word_definition(word)
